4184bd6b0f3af494b1ff370fd8d98e58bcd1d352
fibbo <siebesiech@gmail.com>
2013-08-09 12:18:11 +0200

    conjugate gradient method completed
    hardcoded example added

--- main.cpp
+++ main.cpp
@@ -10,12 +10,13 @@
 #include <boost/numeric/ublas/matrix.hpp>
 #include <boost/numeric/ublas/vector.hpp>
 #include <boost/numeric/ublas/io.hpp>
+#include <math.h>
 #include "DefinedFunctions.cpp"
 
 
 using namespace boost::numeric::ublas;
 double step(double (*func)(double), double x, double h);
-void congrad(boost::numeric::ublas::vector<double> x, boost::numeric::ublas::matrix<double> A, boost::numeric::ublas::vector<double> b);
+void congrad(boost::numeric::ublas::vector<double>& x, boost::numeric::ublas::matrix<double> A, boost::numeric::ublas::vector<double> b);
 int main()
 {
 
@@ -41,14 +42,24 @@ int main()
     }
     std::cout << A << std::endl;
     double z;
-    x(0) = 1;
-    x(1) = 2;
+    x(0) = 2.;
+    x(1) = 1.;
     y(0) = 1;
     y(1) = 2;
     z = inner_prod(x,y);
     w = prod(A,x);
     std::cout << z << std::endl;
     std::cout << w << std::endl;
+
+    A(0,0) = 4.;
+    A(0,1) = 1.;
+    A(1,0) = 1.;
+    A(1,1) = 3.;
+    y(0) = 1.;
+    y(1) = 2.;
+    congrad(x, A, y);
+
+    std::cout << x << std::endl;
     return 0;
 }
 
@@ -56,11 +67,34 @@ double step(double (*func)(double), double x, double h) {
         return x-h*(*func)(x);
 }
 
-void congrad(boost::numeric::ublas::vector<double> x, boost::numeric::ublas::matrix<double> A, boost::numeric::ublas::vector<double> b) {
+void congrad(boost::numeric::ublas::vector<double>& x, boost::numeric::ublas::matrix<double> A, boost::numeric::ublas::vector<double> b) {
   using namespace boost::numeric::ublas;
+  double epsilon = 0.001;
   unsigned int i_max = 10000;
   unsigned int i = 0;
   vector<double> r ((int)(x.size()));
   r = b - prod(A,x);
+  vector<double> d = r;
+  double delta_new = inner_prod(r,r);
+  double delta_0 = delta_new;
+
+  while (i < i_max && delta_new > pow(epsilon,2)) {
+    vector<double> q = prod(A,d);
+    double alpha = delta_new/inner_prod(d,q);
+    x = x + alpha*d;
+
+    if (i%50 == 0)
+      r = b - prod(A,x);
+    else
+      r = r - alpha*q;
+
+    double delta_old = delta_new;
+    delta_new = inner_prod(r,r);
+    double beta = delta_new/delta_old;
+    d = r + beta*d;
+    i += 1;
+
+  }
+  std::cout << "number of steps: " << i << std::endl;
 
 }
