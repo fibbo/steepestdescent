769a149c380f1aed8c9341123682d00ff3432a79
fibbo <siebesiech@gmail.com>
2013-08-14 17:04:33 +0200

    added functions

--- algorithms.hpp
+++ algorithms.hpp
@@ -4,42 +4,62 @@
 #include <boost/numeric/ublas/matrix.hpp>
 #include <boost/numeric/ublas/vector.hpp>
 #include <boost/numeric/ublas/io.hpp>
-#include <math.h>
 
 using namespace boost::numeric::ublas;
 
-double step(double (*func)(double), vector<double>& x, vector<double> b, matrix<double> A) {
-  vector<double> r = b-prod(A,x);
-  double alpha = inner_prod(r,r)/inner_prod(r, prod(A,x));
-  x = x + alpha*r; 
-}
-
-void steepdesc(vector<double>& x, matrix<double> A, vector<double> b) {
-  using namespace boost::numeric::ublas;
-  double epsilon = 0.001;
-  unsigned int i_max = 10000;
+void fillmatrix(matrix<double>&, vector<double>);
+void step(vector<double>& x, vector<double> b, matrix<double> A) {
+  double epsilon = 0.000001;
   unsigned int i = 0;
-  vector<double> r ((int)(x.size()));
-  r = b - prod(A,x);
+  unsigned int i_max = 100;
+  vector<double> r = b - prod(A,x);
   double delta = inner_prod(r,r);
-  double delta_0 = delta;
-  while (i < i_max && delta > pow(epsilon,2)) {
+  double delta_0 = delta;  
+  while (i < i_max && delta > pow(epsilon,2)*delta_0) {
     vector<double> q = prod(A,r);
     double alpha = delta/inner_prod(r,q);
     x = x + alpha*r;
-    if (i%50==0)
+    if (i%50 == 0)
       r = b - prod(A,x);
     else
-      r = r - alpha*q;
+      r = r - alpha*q; //saves one vector-matrix multiplication but every 50th step we compute the correct residual (see line above)
 
     delta = inner_prod(r,r);
     i += 1;
+
   }
+  std::cout << "Number if iterations i: " << i << std::endl;
 }
 
+void step_nonlin(vector<double> (*deriv)(vector<double>), vector<double>& x) {
+
+  unsigned int i = 0;
+  unsigned int i_max = 100000;
+  double epsilon = 0.0000001;
+  unsigned int k = 0;
+  vector<double> r = -(*deriv)(x); //residual
+  vector<double> d = r; // direction of the search
+  double delta = inner_prod(r,r);
+  double delta_0 = delta;
+  matrix<double> H(2,2); //Hessian matrix
+  while (i<i_max && delta > pow(epsilon,2)*delta_0) {
+    fillmatrix(H,x);
+    vector<double> q = prod(H,r);
+    double alpha = delta/inner_prod(r,q); // find an alpha such that the function along the direction of search gets minimized
+    x = x + alpha*r;    
+    r = -(*deriv)(x);
+    delta= inner_prod(r,r); 
+    i += 1;
+
+  }
+  std::cout << "Number of iterations i: "  << i << std::endl;
+
+}
+
+/* Conjugate gradient method - real residual calculated every 50th step */
 void congrad(boost::numeric::ublas::vector<double>& x, boost::numeric::ublas::matrix<double> A, boost::numeric::ublas::vector<double> b) {
   using namespace boost::numeric::ublas;
-  double epsilon = 0.001;
+  double epsilon = 0.00001;
   unsigned int i_max = 10000;
   unsigned int i = 0;
   vector<double> r = b - prod(A,x);
@@ -68,6 +88,7 @@ void congrad(boost::numeric::ublas::vector<double>& x, boost::numeric::ublas::ma
 
 }
 
+/* Following 4 functions define the entries of the Hessian matrix of the Rosenbrock function. The function fillmatrix fills a matrix with the calculated entries of the matrix */
 
 double A00(vector<double> x) {
   return 800*pow(x[0],2) - 400*pow((x[1]-pow(x[0],2)),2) - 2;
@@ -93,16 +114,18 @@ void fillmatrix(matrix<double>& A, vector<double>x) {
   A(1,1) = A11(x);
 }
 
+/* Nonlinear conjugate gradient method with Newton-Raphson and Fletcher-Reeves */
+
 void nonlin_congrad(vector<double> (*deriv)(vector<double>), vector<double>& x) {
   unsigned int i = 0;
   unsigned int i_max = 100000;
   double epsilon = 0.0000001;
   unsigned int k = 0;
-  vector<double> r = -(*deriv)(x);
+  vector<double> r = -(*deriv)(x); //residual
   vector<double> d = r; // direction of the search
   double delta_new = inner_prod(r,r);
   double delta_0 = delta_new;
-  matrix<double> H(2,2);
+  matrix<double> H(2,2); //Hessian matrix
   while (i<i_max && delta_new > pow(epsilon,2)*delta_0) {
     unsigned int j = 0;
     unsigned int j_max = 1000;
@@ -133,6 +156,7 @@ void nonlin_congrad(vector<double> (*deriv)(vector<double>), vector<double>& x) 
   std::cout << "Number of iterations i: "  << i << std::endl;
 }
 
+/* f' of the Rosenbrock function */
 vector<double> deriv_rosenbrock(vector<double> x) {
   vector<double> result(2);
   result(0) = 2 * (200 * pow(x[0],3) - 200 * x[0] * x[1] + x[0] - 1);
@@ -140,4 +164,11 @@ vector<double> deriv_rosenbrock(vector<double> x) {
   return result;
 }
 
+
+/* Test function, not finished */
+vector<double> deriv_func(vector<double> x) {
+  vector<double> result(2);
+  result(0) = x[0]*cos( 2*x[0] - exp(x[1]) + 1) * cos(pow(x[0],2)/2 - pow(x[1],2)/4 + 3) - 2*sin(2*x[0] - exp(x[1]) + 1)*sin(pow(x[0],2)/2 - pow(x[1],2)/4 + 3);
+  result(1) = exp(x[1]) * sin( 2*x[0] - exp(x[1]) +1  ) * sin( pow(x[0],2)/2 - pow(x[1],2)/4 + 3 ) - 1/2*x[1]*cos( 2*x[0] - exp(x[1]) + 1 ) * cos( pow(x[0],2)/2 - pow(x[1],2)/4 + 3  ) ;
+}
 #endif
--- main.cpp
+++ main.cpp
@@ -26,161 +26,34 @@ int main(int argc, char * argv[])
      unsigned int N = 0;
      std::cout << "Enter the dimensionality of the matrix" << std::endl;
      std::cin >> N;
-    // matrix<double> A(N,N);
+     matrix<double> A(N,N);
      vector<double> x(N);
-    // vector<double> b(N);
-    // std::ifstream matrixfile(argv[1]);
-    // std::ifstream bfile(argv[2]);
-    // for (int i = 0; i<N; i++) {
-    //   for (int j = 0; j<N; j++) {
-    // 	matrixfile >> A(i,j);
-    //   }
-    // }
-    // matrixfile.close();
+     vector<double> b(N);
+     std::ifstream matrixfile(argv[1]);
+     std::ifstream bfile(argv[2]);
+     for (int i = 0; i<N; i++) {
+       for (int j = 0; j<N; j++) {
+     	matrixfile >> A(i,j);
+       }
+     }
+     matrixfile.close();
 
-    // for (int i = 0; i<N; i++) {
-    //   bfile >> b(i);
-    // }
+     for (int i = 0; i<N; i++) {
+       bfile >> b(i);
+     }
+
+
+     // std::cout << "Enter x*: " << std::endl;
+     // std::cin >> x(0) >> x(1);
+     
+     // nonlin_congrad(deriv_rosenbrock, x);
+     // std::cout << x << std::endl;
+     
      std::cout << "Enter x*: " << std::endl;
      std::cin >> x(0) >> x(1);
-
-    // double z;
-    //fillmatrix(A,x);
-    //congrad_2(x, A, b);
-
-    nonlin_congrad(deriv_rosenbrock, x);
-    std::cout << x << std::endl;
-    return 0;
+     
+     step_nonlin(deriv_rosenbrock, x);
+     std::cout << x << std::endl;
+     
+     return 0;
 }
-
-// double step(double (*func)(double), vector<double>& x, vector<double> b, matrix<double> A) {
-//   vector<double> r = b-prod(A,x);
-//   double alpha = inner_prod(r,r)/inner_prod(r, prod(A,x));
-//   x = x + alpha*r; 
-// }
-
-// void steepdesc(boost::numeric::ublas::vector<double>& x, boost::numeric::ublas::matrix<double> A, boost::numeric::ublas::vector<double> b) {
-//   using namespace boost::numeric::ublas;
-//   double epsilon = 0.001;
-//   unsigned int i_max = 10000;
-//   unsigned int i = 0;
-//   vector<double> r ((int)(x.size()));
-//   r = b - prod(A,x);
-//   double delta = inner_prod(r,r);
-//   double delta_0 = delta;
-//   while (i < i_max && delta > pow(epsilon,2)) {
-//     vector<double> q = prod(A,r);
-//     double alpha = delta/inner_prod(r,q);
-//     x = x + alpha*r;
-//     if (i%50==0)
-//       r = b - prod(A,x);
-//     else
-//       r = r - alpha*q;
-
-//     delta = inner_prod(r,r);
-//     i += 1;
-//   }
-// }
-
-// void congrad(boost::numeric::ublas::vector<double>& x, boost::numeric::ublas::matrix<double> A, boost::numeric::ublas::vector<double> b) {
-//   using namespace boost::numeric::ublas;
-//   double epsilon = 0.001;
-//   unsigned int i_max = 10000;
-//   unsigned int i = 0;
-//   vector<double> r ((int)(x.size()));
-//   r = b - prod(A,x);
-//   vector<double> d = r;
-//   double delta_new = inner_prod(r,r);
-//   double delta_0 = delta_new;
-
-//   while (i < i_max && delta_new > pow(epsilon,2)) {
-//     vector<double> q = prod(A,d);
-//     double alpha = delta_new/inner_prod(d,q);
-//     x = x + alpha*d;
-
-//     if (i%50 == 0)
-//       r = b - prod(A,x);
-//     else
-//       r = r - alpha*q;
-
-//     double delta_old = delta_new;
-//     delta_new = inner_prod(r,r);
-//     double beta = delta_new/delta_old;
-//     d = r + beta*d;
-//     i += 1;
-
-//   }
-//   std::cout << "number of steps: " << i << std::endl;
-
-// }
-
-
-// void fillmatrix(matrix<double>& A, vector<double>x) {
-//   A(0,0) = A00(x);
-//   A(0,1) = A01(x);
-//   A(1,0) = A10(x);
-//   A(1,1) = A11(x);
-// }
-
-// double A00(vector<double> x) {
-//   return 800*pow(x[0],2) - 400*pow((x[1]-pow(x[0],2)),2) - 2;
-// }
-
-// double A01(vector<double> x) {
-//   return -400*x[0];
-// }
-
-// double A10(vector<double> x) {
-//   return -400*x[0];
-// }
-
-// double A11(vector<double> x) {
-//   return 200;
-// }
-
-// void nonlin_congrad(vector<double> (*deriv)(vector<double>), vector<double>& x) {
-//   unsigned int i = 0;
-//   unsigned int i_max = 100000;
-//   double epsilon = 0.0001;
-//   unsigned int k = 0;
-//   vector<double> r = -(*deriv)(x);
-//   vector<double> d = r; // direction of the search
-//   double delta_new = inner_prod(r,r);
-//   double delta_0 = delta_new;
-//   matrix<double> H(2,2);
-//   while (i<i_max && delta_new > pow(epsilon,2)*delta_0) {
-//     unsigned int j = 0;
-//     unsigned int j_max = 100;
-//     double delta_d = inner_prod(d,d);
-//     double alpha; // find an alpha such that the function along the direction of search gets minimized
-
-//     do {
-//     fillmatrix(H,x);
-//       alpha = -inner_prod((*deriv)(x),d) / inner_prod( d, prod( H , d) );
-//       x = x + alpha*d;
-//       j += 1;
-//     } while (j < j_max && pow(alpha,2) * delta_d > pow(epsilon,2));
-
-//     r = -(*deriv)(x);
-//     double delta_old = delta_new;
-//     delta_new = inner_prod(r,r);
-//     double beta = delta_new/delta_old;
-//     d = r + beta*d;
-//     k += 1;
-//     if (k = 100 || inner_prod(r,d) <= 0) {
-//       d = r;
-//       k = 0;
-//     }
-
-//     i += 1;
-
-//   }
-//   std::cout << "Number of iterations i: "  << i << std::endl;
-// }
-
-// vector<double> deriv_rosenbrock(vector<double> x) {
-//   vector<double> result(2);
-//   result(0) = 2 * (200 * pow(x[0],3) - 200 * x[0] * x[1] + x[0] - 1);
-//   result(1) = 200*(x[1] - pow(x[0],2));
-//   return result;
-// }
